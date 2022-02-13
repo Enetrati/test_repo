40608961d60544508b6db9b1ad9525e683ab115d
Chris Dorsman <c.dorsman@gmail.com>
2022-02-13 17:02:46 +0100

    Added pass to main for Flake8

--- test_repo/cli.py
+++ test_repo/cli.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python3
 """Console script for test_repo."""
 
-from sys import exit
-from platform import python_version, machine, system
-from os import utime, makedirs
-from os.path import exists, dirname, abspath, join
-
 from logging import getLogger
 from logging.config import fileConfig
-
+from os import makedirs, utime
+from os.path import abspath, dirname, exists, join
+from platform import machine, python_version, system
+from sys import exit
 
 
 def main():
-    """ Main """
+    """Main"""
+    pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     __loggername__ = "test_repo"
-    fileConfig(fname={}).format(
-        join(
-            dirname(dirname(abspath(__file__))),
-            'conf')
-    )
+    fileConfig(fname={}).format(join(dirname(dirname(abspath(__file__))), "conf"))
     log = getLogger(__loggername__)
-    log.debug('%s-%s running on Python version: %s-%s on %s',
+    log.debug(
+        "%s-%s running on Python version: %s-%s on %s",
         __loggername__,
-        '0.1.0',
+        "0.1.0",
         python_version(),
         machine(),
-        system())
-    log.debug('Configuration settings:\n\n \
+        system(),
+    )
+    log.debug(
+        "Configuration settings:\n\n \
 Approot: %s |\n \
 Logconfig file: %s |\n \
-Config file: %s |\n',
-                  parser.app_root,
-                  parser.default_logconf,
-                  parser.config_file)
+Config file: %s |\n",
+        parser.app_root,
+        parser.default_logconf,
+        parser.config_file,
+    )
 
     main()
